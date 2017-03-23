a7b43662e805f1f9a86b0335af406c24e22c2bcd
Matthew Wu <mewu@caltech.edu>
2017-03-23 18:01:06 -0500

    Resource transfer action

--- card_action.py
+++ card_action.py
@@ -45,6 +45,22 @@ class ResourceChange():
             opponent.change_resource(self.resource, self.amount)
 
 
+class ResourceTransfer():
+    def __init__(self, resource, amount):
+        """
+        Construct a resource transfer action in favor of the card player.
+        :param resource: Resource to transfer.
+        :param amount: Positive integer amount; transfer at most this amount.
+        """
+        self.resource = resource
+        assert self.amount >= 1
+        self.amount = amount
 
+    def _play(self, player, opponent):
+        """Apply the resource transfer."""
+        transfer_amount = min(opponent.resources[self.resource], self.amount)
+        # Move 'transfer_amount' stocks from opponent to player
+        opponent.change_resource(self.resource, -transfer_amount)
+        player.change_resource(self.resource, transfer_amount)
 
 
