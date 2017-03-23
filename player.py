43875ea58173b41389e8fecec46d143cd1685865
Matthew Wu <mewu@caltech.edu>
2017-03-23 18:14:02 -0500

    Player lose_resource() and attacked()

--- player.py
+++ player.py
@@ -8,7 +8,31 @@ class Player():
 
     def change_resource(self, resource, amount):
         """
-        Change resource by the specified amount. If the player's resource would
-        go below 0, cap at 0.
+        Gain or lose some of a resource; it won't go below 0.
+        :param resource: Resource to change.
+        :param amount: Integer amount.
         """
         self.resources[resource] = min(0, self.resources[resource] + amount)
+
+    def lose_resource(self, resource, amount):
+        """
+        Lose some of a resource; it won't go below 0.
+        :param resource: Resource to lose.
+        :param amount: Nonnegative integer amount; lose at most this much.
+        """
+        assert amount >= 0
+        self.change_resource(resource, -amount)
+
+    def attacked(self, amount):
+        """
+        Get attacked by the specified amount. First lose the fence, then
+        the castle. Neither will go below 0.
+        :param amount: Positive integer attack amount.
+        """
+        assert amount >= 1
+        if self.resources['F'] >= amount:
+            self.resources['F'] -= amount
+        else:
+            amount_left = amount - self.resources['F']
+            self.resources['F'] -= amount
+            self.lose_resource('C', amount_left)
